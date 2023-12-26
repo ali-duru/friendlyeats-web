35bf7234b6fcee79a84b4bc95025c4b37ef8d2ef
Ali Duru <aliduruu06@gmail.com>
2023-12-26 13:09:47 +0300

    firebase

--- nextjs-start/src/lib/firebase/config.js
+++ nextjs-start/src/lib/firebase/config.js
@@ -1,7 +1,8 @@
 export default {
-	projectId: "demo-codelab-nextjs",
-	appId: "demo-codelab-nextjs",
-	apiKey: "demo-codelab-nextjs",
-	storageBucket: "demo-codelab-nextjs.appspot.com",
-	authDomain: "demo-codelab-nextjs.firebaseapp.com",
+	apiKey: 'AIzaSyD19OKS7yN2EUTmBfXAlRSMSQH980Hrhf8',
+	authDomain: 'friendlyeats-codelab-6fcc8.firebaseapp.com',
+	projectId: 'friendlyeats-codelab-6fcc8',
+	storageBucket: 'friendlyeats-codelab-6fcc8.appspot.com',
+	messagingSenderId: '270849978468',
+	appId: '1:270849978468:web:60fe9533524a0158e52f71',
 };
